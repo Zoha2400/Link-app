6776ba10b5cf40a82097ee096a0824eb56df548c
Zohir <vasv29834@gmail.com>
2024-04-19 23:25:42 +0500

    Buttons

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Info() {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full relative text-black">
       <div className="pers-info flex justify-center items-center gap-3 flex-col pt-5">
         <div className="img w-80 h-80 bg-gray-600 rounded-full">
           <img src="" alt="" />
@@ -17,11 +17,18 @@ function Info() {
           </p>
         </div>
       </div>
-      <div className="info text-black flex justify-center items-center flex-col *:text-start *:w-10/12 mt-10 *:text-lg *:cursor-pointer *:duration-100">
+      <div className="info  flex justify-center items-center flex-col *:text-start *:w-10/12 mt-10 *:text-lg *:cursor-pointer *:duration-100">
         <p className="text-blue-500 hover:ml-5">Links - 1564</p>
         <p className="text-green-500 hover:ml-5">Messages - 5465</p>
         <p className="text-gray-500 hover:ml-5">Pictures - 12</p>
         <p className="text-red-400 hover:ml-5">Files - 57</p>
+      </div>
+
+      <div className="buttons absolute flex justify-center gap-2 text-lg items-center flex-col bottom-0 p-5 z-30  w-full h-auto">
+        <button className="change w-10/12 bg-gray-300 rounded-lg">FAQ</button>
+        <button className="change w-10/12 bg-red-400 text-white rounded-lg">
+          Quit account
+        </button>
       </div>
     </div>
   );
