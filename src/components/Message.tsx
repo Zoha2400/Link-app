0dd6908d8f52a85258454ec1321e6edf5bb447b2
Zohir <vasv29834@gmail.com>
2024-04-21 12:42:50 +0500

    Redux-tolkit connected

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,30 +1,34 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 function Message() {
   const [startX, setStartX] = useState(null);
   const [marginLeft, setMarginLeft] = useState(0);
 
-  function handleMouseDown(event) {
+  function handleMouseDown(event: any) {
     setStartX(event.clientX); // Сохраняем начальную позицию по оси X
   }
 
-  function handleMouseMove(event) {
+  function handleMouseMove(event: any) {
     if (startX !== null) {
       const distance = event.clientX - startX; // Вычисляем расстояние, на которое переместили мышь
-      if (distance < 0 && distance > -100) {
+      if (distance < 0 && distance > -101) {
         setMarginLeft(distance);
       }
     }
   }
 
   function handleMouseUp() {
-    setStartX(null); // Сбрасываем начальную позицию после отпускания кнопки мыши
-    setMarginLeft(0);
+    // Сбрасываем начальную позицию после отпускания кнопки мыши
+
+    if (marginLeft !== -100) {
+      setMarginLeft(0);
+      setStartX(null);
+    }
   }
 
   return (
     <div
-      className="w-full pt-5 pb-5 p-10 bg-white mt-5 text-xl flex justify-center items-start flex-col duration-300"
+      className="w-full pt-5 pb-5 p-10 bg-white mt-5 text-xl flex justify-center items-start flex-col duration-200"
       onMouseDown={handleMouseDown}
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
