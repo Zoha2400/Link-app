2ae4a281c8173e5fd1ac71a1db536f6ab45e14ad
Zohir <vasv29834@gmail.com>
2024-04-21 11:13:19 +0500

    replying animation

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,6 +1,36 @@
+import React, { useState } from "react";
+
 function Message() {
+  const [startX, setStartX] = useState(null);
+  const [marginLeft, setMarginLeft] = useState(0);
+
+  function handleMouseDown(event) {
+    setStartX(event.clientX); // Сохраняем начальную позицию по оси X
+  }
+
+  function handleMouseMove(event) {
+    if (startX !== null) {
+      const distance = event.clientX - startX; // Вычисляем расстояние, на которое переместили мышь
+      if (distance < 0 && distance > -100) {
+        setMarginLeft(distance);
+      }
+    }
+  }
+
+  function handleMouseUp() {
+    setStartX(null); // Сбрасываем начальную позицию после отпускания кнопки мыши
+    setMarginLeft(0);
+  }
+
   return (
-    <div className="w-full pt-5 pb-5 p-10 bg-white mt-5 text-xl flex justify-center items-start flex-col">
+    <div
+      className="w-full pt-5 pb-5 p-10 bg-white mt-5 text-xl flex justify-center items-start flex-col duration-75"
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onMouseOut={handleMouseUp}
+      style={{ marginLeft: `${marginLeft}px` }}
+    >
       <div className="text">Message</div>
       <div className="date text-sm">14:15</div>
     </div>
